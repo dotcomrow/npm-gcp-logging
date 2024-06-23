f5f66ee3920d6df8e75ced85180876bd63591c9e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-22 20:07:15 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,17 @@ import axios from 'axios';
 import GetAccessToken from './getGoogleAccessToken';
 
 class GCloudLogger {
-  private projectId: string;
-  private keyfile: string;
-
-  public constructor(projectId: string, keyFilePath: string) {
-    this.projectId = projectId;
-    this.keyfile = keyFilePath;
-  }
-
-  public async logEntry(logName: string, severity: string, message: string): Promise<void> {
+  public static async logEntry(projectId: string, keyfile: string, logName: string, severity: string, message: string) {
     const url = `https://logging.googleapis.com/v2/entries:write`;
     
     const scope = 'https://www.googleapis.com/auth/logging.write'; // replace with the desired scope
 
 
-    const gcloudAuth = new GetAccessToken(this.projectId, this.keyfile);
+    const gcloudAuth = new GetAccessToken(projectId, keyfile);
     const accessToken = await gcloudAuth.getAccessToken(scope);
 
     const logEntry = {
-      logName: `projects/${this.projectId}/logs/${logName}`,
+      logName: `projects/${projectId}/logs/${logName}`,
       resource: {
         type: 'global',
       },
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,10 +4,9 @@ import * as fs from 'fs';
 const projectId = 'gcploggingproject-427121'; // replace with your GCP project ID    
 const keyFilePath = '/Users/admin/Downloads/logging-test-key.json'; // replace with the path to your service account key file
 const keyFileContent = fs.readFileSync(keyFilePath, 'utf8');
-const logger = new GCloudLogger(projectId, keyFileContent);
 
 const logName = 'my-log';
 const severity = 'INFO';
 const message = 'This is a log message test log13.';
 
-logger.logEntry(logName, severity, message);
+GCloudLogger.logEntry(projectId, keyFileContent, logName, severity, message);
